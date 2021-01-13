9667b8d5d9507109b7a0389e30540a84636a636a
진현우 <49802982+jhw3979@users.noreply.github.com>
2021-01-14 02:44:16 +0900

    prac02
    수학적 사고방식을 가지도록 식을 단축하였고
    반복을 줄이는방법을 공부하였습니다.

--- src/main/example/chaper03/prac02.py
+++ src/main/example/chaper03/prac02.py
@@ -15,7 +15,6 @@ for i in range(0, len(num)):
 temp1 = num[0]
 temp2 = num[1]
 
-count = 0
 answer = 0
 
 for i in range(1, m+1):
@@ -24,6 +23,48 @@ for i in range(1, m+1):
     else:
         answer += temp1
 
+print(answer)
+
+#마무리 시간
+end_time = time.time()
+print("WorkingTime: {} sec".format(end_time-start_time))
+
+#시작 시간
+start_time = time.time()
+
+num.sort()
+
+temp1 = num[n - 1]
+temp2 = num[n - 2]
+
+answer = 0
+
+for i in range(1, m+1):
+    if i % k == 0:
+        answer += temp2
+    else:
+        answer += temp1
+
+print(answer)
+
+#마무리 시간
+end_time = time.time()
+print("WorkingTime: {} sec".format(end_time-start_time))
+
+#시작 시간
+start_time = time.time()
+
+num.sort()
+
+temp1 = num[n - 1]
+temp2 = num[n - 2]
+
+count = int(m/(k+1)) * k
+count += m % (k+1)
+
+answer = 0
+answer += (count) * temp1
+answer += (m - count) * temp2
 print(answer)
 
 #마무리 시간
